59203bb5cce2f6e898ad76eb42b00fb4f06197a8
leerlandais <leerlandais@gmail.com>
2024-09-18 22:25:15 +0100

    sleep before I break something

--- controller/publicController.php
+++ controller/publicController.php
@@ -3,7 +3,7 @@
 $route = $_GET['route'] ?? 'home';
 switch ($route) {
     case 'home':
-        echo $twig->render('template.html.twig');
+        echo $twig->render('publicView/public.home.html.twig');
         break;
 
     default:
