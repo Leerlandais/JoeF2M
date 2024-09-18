ed62cb3d2827260120d4a17006bb59faa4effdeb
leerlandais <leerlandais@gmail.com>
2024-09-18 19:09:04 +0100

    layout: temporarily directs to template.twig while testing footer (footer: ok) and header (header: links not active)

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,12 @@
 <?php
+
+$route = $_GET['route'] ?? 'home';
+switch ($route) {
+    case 'home':
+        echo $twig->render('template.html.twig');
+        break;
+
+    default:
+        echo $twig->render('publicView/public.404.html.twig');
+        break;
+}
