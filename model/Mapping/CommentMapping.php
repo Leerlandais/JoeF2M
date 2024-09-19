1e9d497e8a3b04f62c0b55e0536ab003c29f8d60
leerlandais <leerlandais@gmail.com>
2024-09-19 20:06:06 +0100

    Mapping: added Getters/Setters to Comment

--- model/Mapping/CommentMapping.php
+++ model/Mapping/CommentMapping.php
@@ -12,5 +12,67 @@ class CommentMapping extends AbstractMapping
     protected ?string $joe_comm_tele = null;
     protected ?string $joe_comm_text = null;
     protected ?bool $joe_comm_vis = null;
+
+    public function getJoeCommId(): ?int
+    {
+        return $this->joe_comm_id;
+    }
+
+    public function setJoeCommId(?int $joe_comm_id): void
+    {
+        $this->joe_comm_id = $joe_comm_id;
+    }
+
+    public function getJoeCommNom(): ?string
+    {
+        return $this->joe_comm_nom;
+    }
+
+    public function setJoeCommNom(?string $joe_comm_nom): void
+    {
+        $this->joe_comm_nom = $joe_comm_nom;
+    }
+
+    public function getJoeCommMail(): ?string
+    {
+        return $this->joe_comm_mail;
+    }
+
+    public function setJoeCommMail(?string $joe_comm_mail): void
+    {
+        $this->joe_comm_mail = $joe_comm_mail;
+    }
+
+    public function getJoeCommTele(): ?string
+    {
+        return $this->joe_comm_tele;
+    }
+
+    public function setJoeCommTele(?string $joe_comm_tele): void
+    {
+        $this->joe_comm_tele = $joe_comm_tele;
+    }
+
+    public function getJoeCommText(): ?string
+    {
+        return $this->joe_comm_text;
+    }
+
+    public function setJoeCommText(?string $joe_comm_text): void
+    {
+        $this->joe_comm_text = $joe_comm_text;
+    }
+
+    public function getJoeCommVis(): ?bool
+    {
+        return $this->joe_comm_vis;
+    }
+
+    public function setJoeCommVis(?bool $joe_comm_vis): void
+    {
+        $this->joe_comm_vis = $joe_comm_vis;
+    }
+
     
-}
+
+} // end class
