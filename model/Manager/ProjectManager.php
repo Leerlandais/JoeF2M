26312d71643f33732a0d23e898c32c93e23ef6c7
leerlandais <leerlandais@gmail.com>
2024-09-19 06:59:37 +0100

    Manager: added function for random project retrieval

--- model/Manager/ProjectManager.php
+++ model/Manager/ProjectManager.php
@@ -3,8 +3,24 @@
 namespace model\Manager;
 
 use model\Abstract\AbstractManager;
+use model\Mapping\ProjectMapping;
 
 class ProjectManager extends AbstractManager
 {
-    
-}
+    public function getRandomProjects($limit = 9): array|bool
+    {
+        $query = $this->db->query(
+            "SELECT * FROM joe_projects WHERE joe_proj_vis = 1 ORDER BY RAND() LIMIT {$limit}"
+        );
+        $projects = $query->fetchAll();
+        $query->closeCursor();
+        $projObject = [];
+        foreach ($projects as $project) {
+            $projObject[] = new ProjectMapping($project);
+        }
+        return $projObject;
+    }
+
+
+
+} // end class
