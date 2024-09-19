4d81e2349473b4756cc27a991221befc8e1f18e1
leerlandais <leerlandais@gmail.com>
2024-09-19 06:40:28 +0100

    Mapping: added Project mapping properties

---
+++ model/Mapping/ProjectMapping.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace model\Mapping;
+
+use model\Abstract\AbstractMapping;
+
+class ProjectMapping extends AbstractMapping
+{
+protected ?int $joe_proj_id = null;
+protected ?string $joe_proj_title = null;
+protected ?string $joe_proj_desc = null;
+protected ?string $joe_proj_img_loc = null;
+protected ?string $joe_proj_url = null;
+protected ?bool $joe_proj_vis = null;
+}
