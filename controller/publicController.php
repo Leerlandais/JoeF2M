c3ee469f06469ae79f403cec5e6976a7b3118fea
leerlandais <leerlandais@gmail.com>
2024-09-20 10:35:05 +0200

    created function call for contact

--- controller/publicController.php
+++ controller/publicController.php
@@ -2,9 +2,30 @@
 
 use model\Manager\ProjectManager;
 use model\Manager\CommentManager;
+use model\Mapping\CommentMapping;
 $projectManager = new ProjectManager($db);
 $commentManager = new CommentManager($db);
 
+
+// ADD NEW CODE
+if (isset(
+    $_POST["contactFormUser"],
+    $_POST["contactFormMail"],
+    $_POST["contactFormTele"],
+    $_POST["contactFormText"]
+)) {
+    die(var_dump($_POST));
+    $commMapData = [
+        'joe_comm_nom' => $_POST["contactFormUser"],
+        'joe_comm_mail' => $_POST["contactFormMail"],
+        'joe_comm_tele' => $_POST["contactFormTele"],
+        'joe_comm_mess' => $_POST["contactFormText"]
+    ];
+    // sanitisation of code is handled during the Mapping by the Setters
+    $codeMapping = new CommentMapping($commMapData);
+    die(var_dump($commMapData));
+    $addCode = $commentManager->addComment($codeMapping);
+}
 
 
 $route = $_GET['route'] ?? 'home';
