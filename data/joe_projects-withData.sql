1814a0ccb4eca56d7fff3efb4e544b76693f8c17
leerlandais <leerlandais@gmail.com>
2024-09-22 15:01:26 +0100

    added new images and sql update (with data)

---
+++ data/joe_projects-withData.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 22, 2024 at 02:00 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `joef2m`
+--
+
+--
+-- Dumping data for table `joe_projects`
+--
+
+INSERT INTO `joe_projects` (`joe_proj_id`, `joe_proj_alt`, `joe_proj_class`, `joe_proj_img_loc`, `joe_proj_url`, `joe_proj_vis`) VALUES
+(1, 'Guillaume Baillieux', 'webDev', '/public/images/projects/prefo_GB.svg', 'https://2023.webdev-cf2m.be/Guillaume/Siteprefo/', 1),
+(2, 'Lee Brennan', 'webDev', '/public/images/projects/prefo_LB.jpeg', 'https://2023.webdev-cf2m.be/Lee/Site/', 1),
+(3, 'Maxi Rojas', 'aniDig', '/public/images/projects/prefo_MR.png', 'https://2023.webdev-cf2m.be/Arnaldo/siteprefo/', 1),
+(4, 'Baris Aktas', 'aniDig', '/public/images/projects/prefo_BA.png', 'https://2023.webdev-cf2m.be/Baris/Preformation%20site/', 1),
+(5, 'Enez Gubeljic', 'webDes', '/public/images/projects/prefo_EG.png', 'https://2023.webdev-cf2m.be/Enez/site%20prefo/', 1),
+(6, 'Erhan Erdas', 'webDev', '/public/images/projects/prefo_EE.png', 'https://2023.webdev-cf2m.be/Erhan/PrefoSiteWeb/', 1),
+(7, 'Anthony Arnone', 'aniDig', '/public/images/projects/prefo_AA.png', 'https://2023.webdev-cf2m.be/Anthony/Siteprefo/', 1),
+(8, 'Julien Wisniewski', 'webDes', '/public/images/projects/prefo_JW.png', 'https://2023.webdev-cf2m.be/Julian/prefowebsite/', 1),
+(9, 'Kevin Vanwassenhove', 'webDes', '/public/images/projects/prefo_KV.png', 'https://2023.webdev-cf2m.be/Kevin/prefowebsite/', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
