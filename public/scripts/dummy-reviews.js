9c4196dbfa7eb4ee2597609539da762488a0a7a6
leerlandais <leerlandais@gmail.com>
2024-09-25 21:54:40 +0100

    json finally working. html at d-none while I resize stuff

--- public/scripts/dummy-reviews.js
+++ public/scripts/dummy-reviews.js
@@ -1,10 +1,36 @@
+// console.log("dummy reviews update"); // This works
 function getReviews() {
-    fetch("dummy-reviews.json")
-        .then(function(response){
-            response.json().then(function(data){
-                postReviews(data);
-            });
+    fetch("scripts/dummy-reviews.json")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+       //     console.log('Fetched data:', data); // Check if data is fetched correctly // this works
+            postReviews(data);
         })
         .catch(error => console.error('Error fetching reviews:', error));
 }
 
+
+function postReviews(datas) {
+ //   console.log('Here is the data:', datas);  // this works
+    const container = document.getElementById('review-container');
+
+    datas.forEach(function(data) {
+        const reviewList = document.createElement("ul");
+        reviewList.classList.add(data.section);
+
+        let LI = document.createElement("li");
+        LI.innerHTML = `<span id="${data.id}">
+                            <span>${data.name}</span>
+                            <span><img src="${data.img}" alt="avatar image ${data.name}" title="${data.license}"></span>
+                            <span>${data.comment}</span>
+                        </span>`;
+
+        reviewList.appendChild(LI);
+
+        container.appendChild(reviewList);
+    });
+}
+
+getReviews();
