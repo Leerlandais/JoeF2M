ef91069c77a2079696114f72f9e7d45fac666a22
leerlandais <leerlandais@gmail.com>
2024-09-26 08:04:08 +0100

    improved formula, now returns random selection

--- public/scripts/dummy-reviews.js
+++ public/scripts/dummy-reviews.js
@@ -5,7 +5,7 @@ function getReviews() {
             return response.json();
         })
         .then(function(data) {
-       //     console.log('Fetched data:', data); // Check if data is fetched correctly // this works
+     //       console.log('Fetched data:', data); // Check if data is fetched correctly // this works
             postReviews(data);
         })
         .catch(error => console.error('Error fetching reviews:', error));
@@ -13,25 +13,27 @@ function getReviews() {
 
 
 function postReviews(datas) {
- //   console.log('Here is the data:', datas);  // this works
+
     const container = document.getElementById('review-container');
-    let i = 0;
-    datas.forEach(function(data) {
+
+    const shuffleDatas = datas.sort(() => 0.5 - Math.random());
+    const limitDatas = shuffleDatas.slice(0, 2);
+
+    limitDatas.forEach(function(data) {
         const reviewList = document.createElement("ul");
         reviewList.classList.add(data.section);
-        let LI = document.createElement("li");
-        if(i < 9) {
-        i++;
 
+        let LI = document.createElement("li");
         LI.innerHTML = `<span id="${data.id}">
                             <span>${data.name}</span>
                             <span><img src="${data.img}" alt="avatar image ${data.name}" title="${data.license}"></span>
                             <span>${data.comment}</span>
                         </span>`;
+
         reviewList.appendChild(LI);
-        }
         container.appendChild(reviewList);
     });
 }
+
 
 getReviews();
